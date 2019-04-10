ab06858396a35a4ada2d47d97e2bb6f88b9d4ac3
Shravan Shetty <shravan.shetty@colorado.edu>
2019-04-09 22:28:15 -0600

    Setting up redux saga for API calls

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
 import rootReducer from './reducers/merge'
-import App from './components/app'
+import App from './components/App'
+import {createStore, applyMiddleware} from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
-const store = createStore(rootReducer)
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('app'));
