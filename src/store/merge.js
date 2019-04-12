2174026825e56d72f53d2586ac14a7a971fb0318
Shravan Shetty <shravan.shetty@colorado.edu>
2019-04-12 00:37:39 -0600

    Initializing the album plugin

--- src/store/merge.js
+++ src/store/merge.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import welcomeReducer from '../WelcomePlugin/reducers/welcomeReducer'
 import searchReducer from "../SearchPlugin/reducers/searchReducer"
 import mapReducer from "../MapPlugin/reducers/mapReducer"
+import albumReducer from "../AlbumPlugin/reducers/albumReducer"
 
 export default combineReducers({
     welcomeReducer,
