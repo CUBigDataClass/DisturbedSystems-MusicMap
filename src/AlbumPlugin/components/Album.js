49fe3728059aac68433de1a56e86b169271fd6ca
Shravan Shetty <shravan.shetty@colorado.edu>
2019-05-03 21:00:11 -0600

    Added a default page Album plugin

--- src/AlbumPlugin/components/Album.js
+++ src/AlbumPlugin/components/Album.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card, Icon, Progress} from 'semantic-ui-react'
+import {Card, Header, Icon, Progress} from 'semantic-ui-react'
 import {connect} from 'react-redux'
 
 class Album extends React.Component {
@@ -7,23 +7,41 @@ class Album extends React.Component {
     render() {
         let artistData = this.props.artistData;
         if (!artistData.spotifyURL) {
-            return (<div className={"albumContainer"}></div>)
-            l
+            return (<div className={"albumContainer"}>
+                <div>
+                    <Header as='h4' icon textAlign='center'>
+                        <Icon name='id card' size="huge" color='blue'/>
+                        <Header.Content>Artist/Song Details</Header.Content>
+                    </Header>
+                </div>
+                <div className={"albumLoading"}>
+                    <Header as='h3' textAlign='center' icon='search' content='Search to find out about the Artist!!'/>
+                </div>
+
+            </div>)
+
         }
         const extra = (
             <Progress indicating label="Popularity" percent={artistData.spotifyPopularity} color='green'/>
         )
 
-        return (
-            <div className={"albumContainer"}>
-                <Card
-                    href={artistData.spotifyURL}
-                    image={artistData.spotifyImage}
-                    header={artistData.artistName}
-                    meta='Artist'
-                    extra={extra}
-                />
-            </div>)
+        return (<div className={"albumContainer"}>
+            <div>
+                <Header as='h4' icon textAlign='center'>
+                    <Icon name='id card' size="huge" color='blue'/>
+                    <Header.Content>Artist/Song Details</Header.Content>
+                </Header>
+            </div>
+            <Card
+                href={artistData.spotifyURL}
+                image={artistData.spotifyImage}
+                header={artistData.artistName}
+                meta='Artist'
+                extra={extra}
+            />
+
+        </div>)
+
     }
 }
 
