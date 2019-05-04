c6c3eec1290bd70ff6781322e7df3a7882df84c9
Shravan Shetty <shravan.shetty@colorado.edu>
2019-05-03 20:10:47 -0600

    Adding the default page for the musicplayer plugin

--- src/MusicPlayer/components/MusicPlayer.js
+++ src/MusicPlayer/components/MusicPlayer.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import SpotifyPlayer from 'react-spotify-player';
+import {Header, Icon} from "semantic-ui-react";
 
 
 class MusicPlayer extends React.Component {
 
     render() {
         let trackData = this.props.data;
+        let trackId =trackData.trackID;
+
+        if(!trackId) {
+            return (<div className={"musicPlayerContainer"}>
+                <div>
+                    <Header as='h4' icon textAlign='center'>
+                        <Icon name='music' size="huge" color='blue'/>
+                        <Header.Content>Music Player</Header.Content>
+                    </Header>
+                </div>
+                <div className={"musicPlayerLoading"}>
+                    <Header as='h3' textAlign='center' icon='search' content='Search for a song and we will play it for you!!' />
+                </div>
+
+            </div>)
+        }
         const size = {
             width: '100%',
             height: '100%',
